44ae8ca742068906e0cdf249e71de59d1ca6eb7b
nuzmulhossainnahid <hasannahidnazmul@gmail.com>
2021-10-16 23:09:25 +0600

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -11,9 +11,9 @@ const kLabelTextStyle = TextStyle(
   color: Color(0xFF8D8E98),
 );
 
-const kNumberTextStyle = TextStyle(
-  fontSize: 50.0,
-  fontWeight: FontWeight.w900,
+// const kNumberTextStyle = TextStyle(
+//   fontSize: 50.0,
+//   fontWeight: FontWeight.w900,
 );
 
 const kLargeButtonTextStyle = TextStyle(
